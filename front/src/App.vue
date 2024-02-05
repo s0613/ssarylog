24defd004d9c4d228efbf9994565f4e97548b4bb
SongSeungJu <wkwjsrj5079@gmail.com>
2024-02-06 02:13:00 +0900

    first vue.js

---
+++ front/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/write">About</RouterLink>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+</style>
